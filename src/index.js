7da55f24b59d96a67430e59545c67c9b6f2759b9
jacobisdev <jacobisdev@gmail.com>
2026-03-22 17:42:13 -0400

    Count how many times a user do a event
    This will allow to print specialized messages
    depending whether the user do 1 or more times
    a specific event

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import fetchJSON from "./utils/fetchJSON"
+import fetchJSON from './utils/fetchJSON.js'
 
 const args = process.argv.slice(2)
 const username = args[0]
@@ -13,4 +13,104 @@ if (args.length !== 1) {
 const userEndpoint = `https://api.github.com/users/${username}/events`
 
 const userData = await fetchJSON(userEndpoint)
-console.log(userData)
+// console.log(userData)
+
+
+
+const eventTypes = [
+	{
+		type: 'CommitCommentEvent',
+		count: 0,
+		message: '',
+	},
+	{
+		type: 'CreateEvent',
+		count: 0,
+		message: '',
+	},
+	{
+		type: 'DeleteEvent',
+		count: 0,
+		message: '',
+	},
+	{
+		type: 'DiscussionEvent',
+		count: 0,
+		message: '',
+	},
+	{
+		type: 'ForkEvent',
+		count: 0,
+		message: 'Forked ? (user/repo)',
+	},
+	{
+		type: 'GollumEvent',
+		count: 0,
+		message: '',
+	},
+	{
+		type: 'IssueCommentEvent',
+		count: 0,
+		message: '',
+	},
+	{
+		type: 'IssuesEvent',
+		count: 0,
+		message: 'Opened a new issue in ? (user/repo)',
+	},
+	{
+		type: 'MemberEvent',
+		count: 0,
+		message: '',
+	}, // Starred user/repo
+	{
+		type: 'PublicEvent',
+		count: 0,
+		message: '',
+	},
+	{
+		type: 'PullRequestEvent',
+		count: 0,
+		message: '',
+	},
+	{
+		type: 'PullRequestReviewEvent',
+		count: 0,
+		message: '',
+	},
+	{
+		type: 'PullRequestReviewCommentEvent',
+		count: 0,
+		message: '',
+	},
+	{
+		type: 'PushEvent',
+		count: 0,
+		message: 'Pushed ? commit/s to ? (user/repo)',
+	},
+	{
+		type: 'ReleaseEvent',
+		count: 0,
+		message: '',
+	},
+	{
+		type: 'WatchEvent',
+		count: 0,
+		message: '',
+	},
+]
+
+userData.forEach((element) => {
+	eventTypes.forEach((event) => {
+		if (element.type === event.type) {
+			event.count++
+		}
+	})
+})
+
+for (const event in eventTypes) {
+	console.log(eventTypes[event].type, eventTypes[event].count)
+}
+
+// console.log(eventTypes)
+// console.log('Output:')
