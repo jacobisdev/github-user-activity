699fbabb42c20d5009cbb17ca6f00f446c65b1c0
jacobisdev <jacobisdev@gmail.com>
2026-03-23 11:02:23 -0400

    print error on non-existing user

--- src/index.js
+++ src/index.js
@@ -15,102 +15,35 @@ const userEndpoint = `https://api.github.com/users/${username}/events`
 const userData = await fetchJSON(userEndpoint)
 // console.log(userData)
 
+// console.log('Output:')
 
+class Event {
+	constructor(type) {
+		this.type = type
+		this.count = 0
+	}
 
-const eventTypes = [
-	{
-		type: 'CommitCommentEvent',
-		count: 0,
-		message: '',
-	},
-	{
-		type: 'CreateEvent',
-		count: 0,
-		message: '',
-	},
-	{
-		type: 'DeleteEvent',
-		count: 0,
-		message: '',
-	},
-	{
-		type: 'DiscussionEvent',
-		count: 0,
-		message: '',
-	},
-	{
-		type: 'ForkEvent',
-		count: 0,
-		message: 'Forked ? (user/repo)',
-	},
-	{
-		type: 'GollumEvent',
-		count: 0,
-		message: '',
-	},
-	{
-		type: 'IssueCommentEvent',
-		count: 0,
-		message: '',
-	},
-	{
-		type: 'IssuesEvent',
-		count: 0,
-		message: 'Opened a new issue in ? (user/repo)',
-	},
-	{
-		type: 'MemberEvent',
-		count: 0,
-		message: '',
-	}, // Starred user/repo
-	{
-		type: 'PublicEvent',
-		count: 0,
-		message: '',
-	},
-	{
-		type: 'PullRequestEvent',
-		count: 0,
-		message: '',
-	},
-	{
-		type: 'PullRequestReviewEvent',
-		count: 0,
-		message: '',
-	},
-	{
-		type: 'PullRequestReviewCommentEvent',
-		count: 0,
-		message: '',
-	},
-	{
-		type: 'PushEvent',
-		count: 0,
-		message: 'Pushed ? commit/s to ? (user/repo)',
-	},
-	{
-		type: 'ReleaseEvent',
-		count: 0,
-		message: '',
-	},
-	{
-		type: 'WatchEvent',
-		count: 0,
-		message: '',
-	},
-]
-
-userData.forEach((element) => {
-	eventTypes.forEach((event) => {
-		if (element.type === event.type) {
-			event.count++
-		}
-	})
-})
-
-for (const event in eventTypes) {
-	console.log(eventTypes[event].type, eventTypes[event].count)
+	increment() {
+		this.count++
+	}
 }
 
-// console.log(eventTypes)
-// console.log('Output:')
+const userEvents = [...new Set(userData.map((e) => e.type))]
+
+// userData.forEach((event) => {
+// 	if (!userEvents.some((e) => e.type === event.type)) {
+// 		userEvents.push(new Event(event.type))
+// 		return
+// 	}
+// 	userEvents.find((e) => e.type === event.type).increment()
+// })
+
+// userData.forEach((event) => {
+// 	userEvents.push()
+// })
+
+console.log(userEvents)
+
+// userEvents.forEach((event) => {
+// 	console.log(event.type, event.count)
+// })
--- src/utils/fetchJSON.js
+++ src/utils/fetchJSON.js
@@ -2,10 +2,14 @@ export default async (url) => {
 	try {
 		const response = await fetch(url)
 		if (!response.ok) {
+			if (response.status === 404) {
+				throw new Error(`User doesn't exists`)
+			}
 			throw new Error(`Response status: ${response.status}`)
 		}
 		return await response.json()
 	} catch (error) {
 		console.error(error.message)
+		process.exit(1)
 	}
 }
